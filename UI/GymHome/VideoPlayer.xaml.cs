adef22175f2f88ff4f12a8ad3f47225918a98e86
pompeucosta <pompeugcosta@gmail.com>
2023-01-02 13:53:18 +0000

    more pages implemented

--- UI/GymHome/VideoPlayer.xaml.cs
+++ UI/GymHome/VideoPlayer.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,28 +15,27 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace GymHome
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class VideoPlayer : Page
     {
-        public MainPage()
+        public VideoPlayer()
         {
             this.InitializeComponent();
+            _mediaPlayerElement.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/video.mp4"));
+            var mediaPlayer = _mediaPlayerElement.MediaPlayer;
+            mediaPlayer.Play();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(ExercisesPage));
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(ExercisesPage));
-        }
+        //protected override async void OnNavigatedTo(NavigationEventArgs e)
+        //{
+        //    var item = (ExerciseItem)e.Parameter;
+        //    _titleTexBlock.Text = item.Title;
+        //}
     }
 }
