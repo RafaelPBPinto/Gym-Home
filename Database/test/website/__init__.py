690900514e4b2a5f4983b9254ccb7a7a978f7264
Tiago-AlvesUA <tiagojba9@ua.pt>
2023-01-29 17:26:07 +0000

    connection to db

--- Database/test/website/__init__.py
+++ Database/test/website/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'QWErty310'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PlanosUser.db'
+
+    db = SQLAlchemy(app)
 
     from .auth import auth
 
--- Database/test/website/auth.py
+++ Database/test/website/auth.py
@@ -1,13 +1,57 @@
-from flask import Blueprint, render_template
+import sqlite3
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/')
+@auth.route('/', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        conn = sqlite3.connect('PlanosUser.db')
+        query = f"SELECT Passe FROM Utilizador WHERE Email = '{email}'"
+        result = conn.execute(query)
+        result = result.fetchone()
+        conn.close()
+        
+        if result:
+            if check_password_hash(result[0], password):
+                flash('Logged in successfully!', category='success')
+                return redirect('/profile')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template('login.html')
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error') 
+        elif len(username) < 3:
+            flash('Username must be greater than 2 characters.', category='error')
+        elif len(password) < 7:
+            flash('Password must be greater than 6 characters.', category='error')
+        else:
+            conn = sqlite3.connect('PlanosUser.db')
+            query = f"INSERT INTO Utilizador (Email, Nome, Passe) VALUES ('{email}', '{username}', '{generate_password_hash(password, method='sha256')}')"
+            conn.execute(query)
+            conn.commit()
+            conn.close()
+
+            flash('Account created!', category='success')
+            
+            return redirect('/profile')
+        
+
     return render_template('register.html')
 
 @auth.route('/profile')
