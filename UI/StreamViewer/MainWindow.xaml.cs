afdae9e826bb8df0884b50d378dd72e452bb3e74
pompeucosta <pompeugcosta@gmail.com>
2023-03-08 20:07:39 +0000

    added support for full youtube links in stream viewer app

--- UI/StreamViewer/MainWindow.xaml.cs
+++ UI/StreamViewer/MainWindow.xaml.cs
@@ -2,19 +2,8 @@
 // Licensed under the MIT License.
 
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Web;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,6 +24,12 @@ namespace StreamViewer
 
         private void ButtonOkClick(object sender, RoutedEventArgs e)
         {
+            if (streamID.Contains("youtube.com"))
+            {
+                var uri = new Uri(streamID);
+                var parameters = HttpUtility.ParseQueryString(uri.Query);
+                streamID = parameters["v"];
+            }
             webview.Source = new Uri($"https://www.youtube.com/embed/{streamID}");
             panel.Visibility = Visibility.Collapsed;
         }
