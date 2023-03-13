986fd0c1ab4f0b8e0f997142e75dc484c733d8f8
Rafael Pinto <RAFAELPBPINTO2@GMAIL.COM>
2023-03-13 11:43:00 +0000

    update batch to open cmds minimized

--- GymHome.bat
+++ GymHome.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
-start cmd.exe /k "cd voice_assistant\rasa && test_voice.bat"
-start cmd.exe /k "cd Database\test && python main.py"
-start cmd.exe /k "cd Cam\reolinkapipyV5\tests && python myTest.py"
-start cmd.exe /k "cd UI\exe && GymHome.exe"
+start /min cmd.exe /k "cd voice_assistant\rasa && test_voice.bat"
+start /min cmd.exe /k "cd Database\test && python main.py"
+start /min cmd.exe /k "cd Cam\reolinkapipyV5\tests && python myTest.py"
+start /min cmd.exe /k "cd UI\exe && GymHome.exe"
